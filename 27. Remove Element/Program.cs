2e288bac204e17517c672c2f879de1127807d8e9
emre erol <emre_erol17@hotmail.com>
2023-09-25 02:44:12 +0300

    Proje dosyası ekle.

---
+++ 27. Remove Element/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        if (nums.Length == 0) return 0;
+        if (nums.Contains(val) == false) return nums.Length;
+        var counter = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            while (nums[i] == val)
+            {
+                counter++;
+                i++;
+                if (counter == nums.Length) { nums = null; return 0; }
+                if (i == nums.Length)
+                    break;
+            }
+            if (i < nums.Length && counter > 0)
+            {
+                for (var j = 0; j < i-counter; j++)
+                    nums[i - counter + j] = nums[i];
+            }
+        }
+        return nums.Length - counter;
+    }
+}
